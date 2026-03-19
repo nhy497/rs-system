10ddbc5f45fa9737e262869921763d0ea8510a8a
Peter NG <nhy040907@gmail.com>
2026-03-19 13:56:22 +0800

    FINAL FINAL: Fix hasOwnProperty error

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -70,7 +70,7 @@ export function deepClone(obj) {
 
   const cloned = {};
   for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
       cloned[key] = deepClone(obj[key]);
     }
   }
